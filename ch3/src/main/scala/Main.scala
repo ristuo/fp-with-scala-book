898d5c0e6f8990e780b833ec7e4dee9072f3a64c
risto <risto.tuomainen1@gmail.com>
2018-03-03 12:22:02 +0200

    3.14 - 3.23

--- ch3/src/main/scala/Main.scala
+++ ch3/src/main/scala/Main.scala
@@ -6,14 +6,20 @@ sealed trait List[+A] {
   def foldLeft[B](z: B)(f: (B, A) => B): B = {
     List.foldLeft(this, z)(f)
   }
+
+  def setHead[B >: A](a: B): List[B] = {
+    Exercise3_3.setHead(a, this)
+  }
   
   def length = {
     List.length(this)
   }
 }
+
 case object Nil extends List[Nothing] {
   override def toString() = ""
 }
+
 case class Cons[+A](head: A, tail: List[A]) extends List[A] {
   override def toString() = head.toString + " " + tail.toString
 }
@@ -62,7 +68,7 @@ object Exercise3_3 {
   def setHead[A](newHead: A, as: List[A]): List[A] = {
     as match { 
       case Nil => List(newHead)
-      case as: Cons[A] => Cons(newHead, as.tail)
+      case as: Cons[A] => Cons(newHead, as)
     }
   }
 }
@@ -136,5 +142,93 @@ object Exercise3_12 {
   }
   def reverseWithFold[T](listy: List[T]): List[T] = {
     listy.foldLeft(Nil: List[T])( (a: List[T], b: T) => Cons(b, a))
+  }
+}
+
+object Exercise3_14 {
+  def append[A](l: List[A], a: A): List[A] = {
+    l.foldRight[List[A]](List[A](a))((b: A, j: List[A]) => Cons[A](b, j))
+  }
+}
+
+object Exercise3_15 {
+  def flatten[A](l: List[List[A]]): List[A] = {
+    l.foldRight[List[A]](Nil)((l1: List[A], l2: List[A]) => {
+      l1.foldRight(l2)((a, listy) => listy.setHead(a))                    
+    })
+  }
+}
+
+object Exercise3_16 {
+  def f(a: List[Int]): List[Int] = {
+    a match {
+      case Nil => Nil
+      case Cons(h, t) => Cons(h + 1, f(t))
+    }
+  }
+}
+
+object Exercise3_17 {
+  def f(a: List[Double]): List[String] = {
+    a match {
+      case Nil => Nil
+      case Cons(h,t) => Cons(h.toString, f(t))
+    }
+  }
+}
+
+object Exercise3_18 {
+  def map[A,B](l: List[A])(f: A => B): List[B] = {
+    l match {
+      case Nil => Nil
+      case Cons(h, t) => Cons(f(h), map(t)(f))
+    }
+  }
+}
+
+object Exercise3_19 {
+  def filter[A](l: List[A])(f: A => Boolean): List[A] = {
+    l match {
+      case Nil => Nil
+      case Cons(h, t) => if (f(h)) Cons(h, filter(t)(f)) else filter(t)(f) 
+    }
+  }
+}
+
+object Exercise3_20 {
+  def flatMap[A,B](l: List[A])(f: A => List[B]): List[B] = {
+    Exercise3_15.flatten(Exercise3_18.map(l)(f))
+  }
+}
+
+object Exercise3_21 {
+  def filter[A](l: List[A])(f: A => Boolean): List[A] = {
+    Exercise3_20.flatMap(l)((a: A) => {
+      if (f(a)) List(a) else Nil
+    })
+  }
+}
+
+object Exercise3_22 {
+  def add(l1: List[Int], l2: List[Int]): List[Int] = {
+    (l1, l2) match { 
+      case (Nil, Nil) => Nil
+      case (x, Nil) => x
+      case (Nil, x) => x
+      case (Cons(a, t1), Cons(b, t2)) => Cons(a + b, add(t1, t2))
+    }
+  }
+}
+
+object Exercise3_23 {
+  def zipWith[A](l1: List[A], 
+                 l2: List[A]
+                )(f: (A, A) => A): List[A] = {
+    (l1, l2) match { 
+      case (Nil, Nil) => Nil
+      case (x, Nil) => x
+      case (Nil, x) => x
+      case (Cons(a, t1), Cons(b, t2)) => Cons(f(a, b), zipWith(t1, t2)(f))
+    }
   }
 }
